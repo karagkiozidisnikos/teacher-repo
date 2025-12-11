7e1556956e8c9348c7c26a9d81a8affdb2d37c95
nkaragkiozidis <karagkiozidis.nikolaos@gmail.com>
2025-12-11 21:13:33 +0200

    feat: add Product with comparator

--- src/gr/aueb/sev/ch18/Main.java
+++ src/gr/aueb/sev/ch18/Main.java
@@ -1,8 +1,31 @@
 package gr.aueb.sev.ch18;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        List<Product> products = new ArrayList<>(List.of(
+                new Product("Apple", 1.2, 10),
+                new Product("Banana", 0.8, 5 )
+        ));
+
+        products.sort(Comparator.naturalOrder()); // Compares based on the comparable on the compareTo
+        products.sort(Comparator.reverseOrder());
+
+        products.sort(Comparator.comparing(Product::getPrice));
+        products.sort(Comparator.comparing(Product::getPrice).reversed());
+        products.sort(Comparator.comparing(Product::getDescription));
+        products.sort(Comparator.comparing(Product::getPrice).thenComparing(Product::getDescription, Comparator.reverseOrder()));
+
+        products.sort(
+                Comparator.comparing(Product::getPrice)
+                        .thenComparing(Product::getDescription)
+                        .thenComparing(Product::getQuantity)
+                        .reversed());
 
         var intNode = new Node<Integer>();
         Node<String> stringNode = new Node<>("Hello");
